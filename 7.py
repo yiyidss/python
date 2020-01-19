b6e08c51e7ed36780bf5af49f2ad8e5857466666
yiyayiyi <1434638193@qq.com>
2020-01-19 17:35:12 +0800

    NB

--- 7.py
+++ 7.py
@@ -1,20 +1,26 @@
-# python--oop
-class Animal():
-    shengao = "gao"
-    tizhong = "zhong"
-    __chiren = "nicai"      # 不可以被继承
-    _suibian = "suibian"       # 可以被继承
-    def elpx(self):
-        print("awuawuawu~")
+# # python--oop
+# class Animal():
+#     shengao = "gao"
+#     tizhong = "zhong"
+#     __chiren = "nicai"      # 不可以被继承
+#     _suibian = "suibian"       # 可以被继承
+#     def elpx(self):
+#         print("awuawuawu~")
+#
+# class Dog(Animal):
+#     shengao = "hengao"
+#     tizhong = "henzhong"
+#     # _suibian = "jiuhensuibian"
+#     def elpx(self):
+#         Animal.elpx(self)
+#         print("wangwangwang")
+#
+# d = Dog()   # 实例化
+# d.elpx()
+# print(d._suibian)
+import calendar
 
-class Dog(Animal):
-    shengao = "hengao"
-    tizhong = "henzhong"
-    # _suibian = "jiuhensuibian"
-    def elpx(self):
-        Animal.elpx(self)
-        print("wangwangwang")
+cal = calendar.calendar(2017)
+print(cal)
 
-d = Dog()   # 实例化
-d.elpx()
-print(d._suibian)
+print(calendar.leapdays(2012,2016))
